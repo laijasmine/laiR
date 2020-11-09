aa126b1f17397483cb42dfa500359b030c415264
laijasmine <jasminelai@nceas.ucsb.edu>
2020-11-09 15:36:49 -0800

    make sure the function accounts for 1 dataTable and multiple dataTables

--- R/annotate_att.R
+++ R/annotate_att.R
@@ -1,6 +1,7 @@
 #' Add attribute annotation to eml document
 #'
 #' @param d eml document
+#' @param index integer a number
 #' @param attribute character - attributeName
 #' @param identifier character - a unique identifier
 #' @param label character - the value semantic annotation label
@@ -10,17 +11,27 @@
 #' @export
 #'
 #' @examples
-annotate_att <- function(d, attribute, identifier, label, valueURI){
-  att <- eml_get_simple(d$dataset$dataTable, "attributeName")
+annotate_att <- function(d, index = NULL, attribute, identifier, label, valueURI){
+  att <- arcticdatautils::eml_get_simple(d$dataset$dataTable, "attributeName")
   j <- which(att == attribute)
 
   #identifier
-  d$dataset$dataTable$attributeList$attribute[[j]]$id <- identifier
+  if(att == 1){ #if there is only one
+    d$dataset$dataTable$attributeList$attribute[[j]]$id <- identifier
 
-  d$dataset$dataTable$attributeList$attribute[[j]]$annotation$propertyURI <- list(label = "contains measurements of type",
-                                                                                  propertyURI = "http://ecoinformatics.org/oboe/oboe.1.2/oboe-core.owl#containsMeasurementsOfType")
-  d$dataset$dataTable$attributeList$attribute[[j]]$annotation$valueURI <- list(label = label,
-                                                                               valueURI = valueURI)
+    d$dataset$dataTable$attributeList$attribute[[j]]$annotation$propertyURI <- list(label = "contains measurements of type",
+                                                                                    propertyURI = "http://ecoinformatics.org/oboe/oboe.1.2/oboe-core.owl#containsMeasurementsOfType")
+    d$dataset$dataTable$attributeList$attribute[[j]]$annotation$valueURI <- list(label = label,
+                                                                                 valueURI = valueURI)
+  } else{
+    d$dataset$dataTable[[i]]$attributeList$attribute[[j]]$id <- identifier
+
+    d$dataset$dataTable[[i]]$attributeList$attribute[[j]]$annotation$propertyURI <- list(label = "contains measurements of type",
+                                                                                    propertyURI = "http://ecoinformatics.org/oboe/oboe.1.2/oboe-core.owl#containsMeasurementsOfType")
+    d$dataset$dataTable[[i]]$attributeList$attribute[[j]]$annotation$valueURI <- list(label = label,
+                                                                                 valueURI = valueURI)
+  }
+
 
   return(d)
 }
