63a3758e0a41be9ac2788b567a4555a3b5b676f9
laijasmine <jasminelai@nceas.ucsb.edu>
2021-01-29 16:10:04 -0800

    change to use lapply instead of a for loop to speed up addHierarchicalDataObject

--- R/addHierarchicalDataObject.R
+++ R/addHierarchicalDataObject.R
@@ -11,6 +11,43 @@ getFormatList <- function(){
 
 }
 
+#' Create new dataObjects based on directory
+#'
+#' @param file_name
+#'
+#' @return
+#' @export
+#'
+#' @examples
+createDataObject <- function(file_name){
+  #get all the names straight
+  ext <- stringr::str_split(file_name, "\\.")
+  fn <- paste0(folder_path, "/", file_name)
+
+  path <- stringr::str_replace(folder_path, pattern, "")
+  fp <- paste0(path, "/", file_name)
+
+  #guess the format
+  formats <- getFormatList()
+  n <- which(formats$Extension == ext[[1]][2])
+
+  if(length(n) > 1){
+    warning(paste0("Format for ", file_name, " is ambiguous. Using application/octet-stream instead"))
+    obj <- new("DataObject", id=paste0("urn:uuid:", UUIDgenerate()), format="application/octet-stream",
+               filename= fn, targetPath = fp)
+  } else if(length(formats$ID[n]) > 0){
+    obj <- new("DataObject", id=paste0("urn:uuid:", UUIDgenerate()), format=formats$ID[n],
+               filename= fn, targetPath = fp)
+  }else{
+    warning(paste0("Format ", file_name, " not found using application/octet-stream"))
+    obj <- new("DataObject", id=paste0("urn:uuid:", UUIDgenerate()), format="application/octet-stream",
+               filename= fn, targetPath = fp)
+  }
+
+  pkg <- addMember(pkg, obj, metadataId) #add data
+
+}
+
 #add an existing data object
 #' Recursively adding an entire folder to a data package
 #'
@@ -23,45 +60,15 @@ getFormatList <- function(){
 #' @export
 #'
 #' @examples
-addHierarchicalDataObject <- function(folder_path, pattern, pkg, metadataId) {
+addHierarchicalDataObject <- function(folder_path, pattern = folder_path, pkg, metadataId) {
   #take in to account if the file path contains a / at the end
   if(stringr::str_detect(folder_path, "/$")){
     folder_path <- stringr::str_remove(folder_path, "/$")
   }
 
   files <- dir(folder_path, recursive = T)
-  progressBar <- utils::txtProgressBar(0, length(files),
-                                       style = 3)
 
-  # Files in data
-  for(i in 1:length(files)){
-    #get all the names straight
-    file_name <- files[[i]]
-    ext <- stringr::str_split(file_name, "\\.")
-    fn <- paste0(folder_path, "/", file_name)
+  lapply(files, createDataObject)
 
-    path <- stringr::str_replace(folder_path, pattern, "")
-    fp <- paste0(path, "/", file_name)
-
-    #guess the format
-    formats <- getFormatList()
-    n <- which(formats$Extension == ext[[1]][2])
-
-    if(length(n) > 1){
-      warning(paste0("Format for ", file_name, " is ambiguous. Using application/octet-stream instead"))
-      obj <- new("DataObject", id=paste0("urn:uuid:", UUIDgenerate()), format="application/octet-stream",
-                 filename= fn, targetPath = fp)
-    } else if(length(formats$ID[n]) > 0){
-      obj <- new("DataObject", id=paste0("urn:uuid:", UUIDgenerate()), format=formats$ID[n],
-                 filename= fn, targetPath = fp)
-    }else{
-      warning(paste0("Format ", file_name, " not found using application/octet-stream"))
-      obj <- new("DataObject", id=paste0("urn:uuid:", UUIDgenerate()), format="application/octet-stream",
-                 filename= fn, targetPath = fp)
-    }
-
-    pkg <- addMember(pkg, obj, metadataId) #add data
-    utils::setTxtProgressBar(progressBar, i)
-  }
   return(pkg)
 }
