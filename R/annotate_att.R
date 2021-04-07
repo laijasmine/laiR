b70e17411570117b080910fc8d1c3e40e6b957bd
laijasmine <jasminelai@nceas.ucsb.edu>
2021-04-07 16:48:00 -0700

    update annotate_att to accommodate spatialVector and spatialRasters

--- R/annotate_att.R
+++ R/annotate_att.R
@@ -1,9 +1,27 @@
+#' Constructs an EML annotation for attributes
+#'
+#' @param label character
+#' @param valueURI URI
+#'
+#' @return EML annotation
+#' @export
+#'
+#' @examples
+annotate <- function(label, valueURI){
+  annotated <- list(propertyURI = list(label = "contains measurements of type",
+                                       propertyURI = "http://ecoinformatics.org/oboe/oboe.1.2/oboe-core.owl#containsMeasurementsOfType"),
+                    valueURI = list(label = label,
+                                    valueURI = valueURI))
+
+  return(annotated)
+}
+
 #' Add attribute annotation to eml document
 #'
 #' @param d eml document
 #' @param index integer a number
 #' @param attribute character - attributeName
-#' @param identifier character - a unique identifier
+#' @param identifier character - a unique identifier (other wise defaults to attribute name)
 #' @param label character - the value semantic annotation label
 #' @param valueURI character- value URI
 #'
@@ -11,30 +29,79 @@
 #' @export
 #'
 #' @examples
-annotate_att <- function (d, index = NULL, attribute, identifier, label, valueURI)
-{
+annotate_att <- function (d, index = NULL, attribute, identifier = NULL, label, valueURI, type = "dataTable"){
+  stopifnot((type %in% c("dataTable", "spatialVector", "spatialRaster")))
 
   if (is.null(index)) {
-    att <- arcticdatautils::eml_get_simple(d$dataset$dataTable,
-                                           "attributeName")
-    j <- which(att == attribute)
+    if(type == "dataTable"){
+      att <- arcticdatautils::eml_get_simple(d$dataset$dataTable,
+                                             "attributeName")
+      j <- which(att == attribute)
+      d$dataset$dataTable$attributeList$attribute[[j]]$id <- identifier
+      d$dataset$dataTable$attributeList$attribute[[j]]$annotation <- annotate(label, valueURI)
+    } else if (type == "spatialVector"){
+      att <- arcticdatautils::eml_get_simple(d$dataset$spatialVector,
+                                             "attributeName")
+      j <- which(att == attribute)
 
-    d$dataset$dataTable$attributeList$attribute[[j]]$id <- identifier
-    d$dataset$dataTable$attributeList$attribute[[j]]$annotation$propertyURI <- list(label = "contains measurements of type",
-                                                                                    propertyURI = "http://ecoinformatics.org/oboe/oboe.1.2/oboe-core.owl#containsMeasurementsOfType")
-    d$dataset$dataTable$attributeList$attribute[[j]]$annotation$valueURI <- list(label = label,
-                                                                                 valueURI = valueURI)
-  }
-  else {
-    att <- arcticdatautils::eml_get_simple(d$dataset$dataTable[[index]],
-                                           "attributeName")
-    j <- which(att == attribute)
+      if(length(att) == 1){
+        d$dataset$spatialVector$attributeList$attribute$id <- identifier
+        d$dataset$spatialVector$attributeList$attribute$annotation <- annotate(label, valueURI)
+      } else {
+        d$dataset$spatialVector$attributeList$attribute[[j]]$id <- identifier
+        d$dataset$spatialVector$attributeList$attribute[[j]]$annotation <- annotate(label, valueURI)
+      }
 
-    d$dataset$dataTable[[index]]$attributeList$attribute[[j]]$id <- identifier
-    d$dataset$dataTable[[index]]$attributeList$attribute[[j]]$annotation$propertyURI <- list(label = "contains measurements of type",
-                                                                                             propertyURI = "http://ecoinformatics.org/oboe/oboe.1.2/oboe-core.owl#containsMeasurementsOfType")
-    d$dataset$dataTable[[index]]$attributeList$attribute[[j]]$annotation$valueURI <- list(label = label,
-                                                                                          valueURI = valueURI)
-  }
+    } else if (type == "spatialRaster"){
+      att <- arcticdatautils::eml_get_simple(d$dataset$spatialRaster,
+                                             "attributeName")
+      j <- which(att == attribute)
+
+      if(length(att) == 1){
+        d$dataset$spatialRaster$attributeList$attribute$id <- identifier
+        d$dataset$spatialRaster$attributeList$attribute$annotation <- annotate(label, valueURI)
+      }else{
+        d$dataset$spatialRaster$attributeList$attribute[[j]]$id <- identifier
+        d$dataset$spatialRaster$attributeList$attribute[[j]]$annotation <- annotate(label, valueURI)
+      }
+
+    }
+
+  } else {
+    if(type == "dataTable"){
+      att <- arcticdatautils::eml_get_simple(d$dataset$dataTable[[index]],
+                                             "attributeName")
+      j <- which(att == attribute)
+
+      d$dataset$dataTable[[index]]$attributeList$attribute[[j]]$id <- identifier
+      d$dataset$dataTable[[index]]$attributeList$attribute[[j]]$annotation <- annotate(label, valueURI)
+    }  else if (type == "spatialVector"){
+      att <- arcticdatautils::eml_get_simple(d$dataset$spatialVector[[index]],
+                                             "attributeName")
+      j <- which(att == attribute)
+
+      if(length(att) == 1){
+        d$dataset$spatialVector[[index]]$attributeList$attribute$id <- identifier
+        d$dataset$spatialVector[[index]]$attributeList$attribute$annotation <- annotate(label, valueURI)
+      } else {
+        d$dataset$spatialVector[[index]]$attributeList$attribute[[j]]$id <- identifier
+        d$dataset$spatialVector[[index]]$attributeList$attribute[[j]]$annotation <- annotate(label, valueURI)
+      }
+
+    } else if (type == "spatialRaster"){
+      att <- arcticdatautils::eml_get_simple(d$dataset$spatialRaster[[index]],
+                                             "attributeName")
+      j <- which(att == attribute)
+
+      if(length(att) == 1){
+        d$dataset$spatialRaster[[index]]$attributeList$attribute$id <- identifier
+        d$dataset$spatialRaster[[index]]$attributeList$attribute$annotation <- annotate(label, valueURI)
+      }else{
+        d$dataset$spatialRaster[[index]]$attributeList$attribute[[j]]$id <- identifier
+        d$dataset$spatialRaster[[index]]$attributeList$attribute[[j]]$annotation <- annotate(label, valueURI)
+      }
+    }}
+
   return(d)
 }
+
